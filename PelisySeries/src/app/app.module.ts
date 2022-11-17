a14bb90849dcd70f6cbc67706e31dce4b0cec70e
Gusy <zyknos@gmail.com>
2022-11-17 11:02:35 +0100

    Creado componente sidebar y añadido a app.component.html

--- PelisySeries/src/app/app.module.ts
+++ PelisySeries/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
