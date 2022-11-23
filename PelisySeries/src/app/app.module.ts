60b6b94b2dea38e52d3b127d106f08bdf3c96ba1
Gusy <zyknos@gmail.com>
2022-11-23 11:37:40 +0100

    Cargado FormsModule en app.module.ts

--- PelisySeries/src/app/app.module.ts
+++ PelisySeries/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -22,6 +23,7 @@ import { HomeComponent } from './components/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [],
