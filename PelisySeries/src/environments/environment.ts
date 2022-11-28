38beec1fba2d004b4471b7cff97c82a6580087cd
Gusy <zyknos@gmail.com>
2022-11-28 11:47:54 +0100

    Creado variables genders, resolutions, codecs y apiUrl en environments.ts

--- PelisySeries/src/environments/environment.ts
+++ PelisySeries/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  genders: ['Acción', 'Ciencia Ficción', 'Comedia', 'Drama', 'Fantasía', 'Melodrama', 'Musical', 'Romance', 'Suspense', 'Terror'],
+  resolutions: ['1080p', '720p'],
+  codecs: ['x265', 'x264'],
+  apiUrl: 'http://localhost:3900/api/'
 };
 
 /*
