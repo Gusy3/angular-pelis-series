c836aef0f356eb60a916a59f9ac6e971c6c638f0
Gusy <zyknos@gmail.com>
2022-12-27 13:03:49 +0100

    Añadido componentes y módulosa app.module.ts

--- PelisySeries/src/app/app.module.ts
+++ PelisySeries/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MomentModule } from 'ngx-moment';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,12 @@ import { MenuComponent } from './components/menu/menu.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { FilmNewComponent } from './components/film-new/film-new.component';
+import { FilmsComponent } from './components/films/films.component';
+import { FilmComponent } from './components/film/film.component';
+import { FilmEditComponent } from './components/film-edit/film-edit.component';
+import { SearchComponent } from './components/search/search.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +26,21 @@ import { HomeComponent } from './components/home/home.component';
     MenuComponent,
     SidebarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    FilmNewComponent,
+    FilmsComponent,
+    FilmComponent,
+    FilmEditComponent,
+    SearchComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MomentModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
