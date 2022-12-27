21c9afb6a9c3585cda550f2a2a0208018fb7da5c
Gusy <zyknos@gmail.com>
2022-12-27 12:56:07 +0100

    Creado componente film-edit

---
+++ PelisySeries/src/app/components/film-edit/film-edit.component.ts
@@ -0,0 +1,118 @@
+import { Component, OnInit } from '@angular/core';
+import { Film } from 'src/app/models/film';
+import { environment } from 'src/environments/environment';
+import { FilmService } from 'src/app/services/film.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import swal from 'sweetalert';
+
+@Component({
+  selector: 'app-film-edit',
+  templateUrl: '../film-new/film-new.component.html',
+  styleUrls: ['./film-edit.component.css'],
+  providers: [FilmService]
+})
+export class FilmEditComponent implements OnInit{
+
+  public subheaderTitle: String;
+  public submitValue: String;
+  public film: Film;
+  public genders: String[];
+  public resolutions: String[];
+  public codecs: String[];
+  public vieweds: String[];
+
+  constructor(
+    private _filmService: FilmService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ){
+    
+    this.subheaderTitle = 'EDITAR PELÍCULA';
+    this.submitValue = 'Editar';
+    this.film = new Film('', '', '', 0, '', '', 0, '', '', '', new Date());
+    this.resolutions = environment.resolutions;
+    this.codecs = environment.codecs;
+    this.genders = environment.genders;
+    this.vieweds = environment.viewed;
+
+  }
+
+  ngOnInit(): void{
+
+    this.getFilm();
+
+  }
+
+  
+  getFilm(){
+
+    this._route.params.subscribe(params=>{
+
+      let id = params['id'];
+
+      this._filmService.getFilm(id).subscribe(
+        
+        response=>{
+
+          if(response.film){
+
+            this.film = response.film;
+
+          }
+          else{
+
+            this._router.navigate(['/peliculas']);
+
+          }
+
+        },
+        error=>{
+
+          console.log(error);
+          this._router.navigate(['/peliculas']);
+
+        }
+      );
+
+    });
+
+
+  }
+
+  onSubmit(){
+
+    console.log(this.film);
+
+    this._filmService.update(this.film._id, this.film).subscribe(
+      response=>{
+
+        if(response){
+
+          this.film = response.film;
+
+          // ALERTA
+
+          swal(
+            '¡¡¡Película editada!!!',
+            'La película se ha editado correctamente',
+            'success'
+          );
+
+          this._router.navigate(['/peliculas']);
+
+        }
+
+      },
+
+      error=>{
+
+        console.log(error);
+
+      }
+
+    );
+
+  }
+
+
+}
