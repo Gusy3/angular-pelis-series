7489216b418417ffb88155dc22afe3fd754d44d4
Gusy <zyknos@gmail.com>
2022-12-27 12:55:47 +0100

    Creado componente film

---
+++ PelisySeries/src/app/components/film/film.component.ts
@@ -0,0 +1,109 @@
+import { Component, OnInit } from '@angular/core';
+import { Film } from 'src/app/models/film';
+import { FilmService } from 'src/app/services/film.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import swal from 'sweetalert';
+
+@Component({
+  selector: 'app-film',
+  templateUrl: './film.component.html',
+  styleUrls: ['./film.component.css'],
+  providers: [FilmService]
+})
+export class FilmComponent implements OnInit{
+
+  public film: Film;
+  public apiUrl: string;
+
+  constructor(
+    private _filmService: FilmService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ){
+    this.film = new Film('', '', '', 0, '', '', 0, '', '', '', new Date());
+    this.apiUrl = environment.apiUrl;
+  }
+
+  ngOnInit(): void{
+
+    this._route.params.subscribe(params=>{
+
+      let id = params['id'];
+
+      this._filmService.getFilm(id).subscribe(
+        
+        response=>{
+
+          if(response.film){
+
+            this.film = response.film;
+
+          }
+          else{
+
+            this._router.navigate(['/home']);
+
+          }
+
+        },
+        error=>{
+
+          console.log(error);
+          this._router.navigate(['/home']);
+
+        }
+      );
+
+    });
+
+  }
+
+  delete(idFilm: String){
+
+    swal({
+      title: "¿Estás seguro?",
+      text: "¡Una vez borrado no podrás recuperarlo!",
+      icon: "warning",
+      buttons: [true, true],
+      dangerMode: true
+    })
+    .then((willDelete)=>{
+      if (willDelete){
+
+        this._filmService.delete(idFilm).subscribe(
+
+          response=>{
+
+            if(response.film){
+
+              swal("¡La película ha sido borrada correctamente!", {
+                icon: "success",
+              });
+              this._router.navigate(['/peliculas']);
+
+            }
+
+          },
+
+          error=>{
+
+            console.log(error);
+            this._router.navigate(['/peliculas']);
+
+          }
+          
+        );
+
+      }else{
+
+        swal("¡Tranquilo nada se ha borrado!");
+
+      }
+
+    });
+
+
+  }
+
+}
