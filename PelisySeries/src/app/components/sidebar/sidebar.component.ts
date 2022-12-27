6bce4ab158068424cb055ec243c52cd6d67eeeff
Gusy <zyknos@gmail.com>
2022-12-27 13:01:55 +0100

    Añadido enlaces a componentes film-new y search

--- PelisySeries/src/app/components/sidebar/sidebar.component.ts
+++ PelisySeries/src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit{
 
-  constructor() { }
+  public title: string;
+
+  constructor(
+    private _router: Router
+  ){
+    this.title = '';
+  }
 
   ngOnInit(): void {
+  }
+
+  goSearch(){
+
+    this._router.navigate(['/buscar', this.title]);
+
   }
 
 }
