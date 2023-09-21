7723d2cc217c547205f16b4c4e268899d253cf54
Gusy <zyknos@gmail.com>
2023-09-21 15:39:55 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  versions: ['Normal', 'Extendida'],
+  genders: ['Acción', 'Animación', 'Aventuras', 'Biográfico', 'Bélico', 'Ciencia Ficción', 'Comedia', 'Crimen', 'Drama', 'Fantastico', 'Infantil', 'Intriga', 'Melodrama', 'Musical', 'Romance', 'Sobrenatural', 'Suspense', 'Terror', 'Thriller', 'Western'],
+  resolutions: ['1080p', '720p'],
+  codecs: ['x265', 'x264'],
+  apiUrl: 'https://pelis-series-backend.onrender.com/api/'
 };
