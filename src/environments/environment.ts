7cd744bab7fd2aa6927a1a21035f5a1ab95834f7
Gusy3 <114913169+Gusy3@users.noreply.github.com>
2023-09-21 15:16:49 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   versions: ['Normal', 'Extendida'],
   genders: ['Acción', 'Animación', 'Aventuras', 'Biográfico', 'Bélico', 'Ciencia Ficción', 'Comedia', 'Crimen', 'Drama', 'Fantastico', 'Infantil', 'Intriga', 'Melodrama', 'Musical', 'Romance', 'Sobrenatural', 'Suspense', 'Terror', 'Thriller', 'Western'],
   resolutions: ['1080p', '720p'],
