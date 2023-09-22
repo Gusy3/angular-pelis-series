d6f8ab3259c9a1667fd052151b30a90c27371160
Gusy <zyknos@gmail.com>
2023-09-22 10:52:24 +0200

    Arreglado ruta de iconos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   genders: ['Acción', 'Animación', 'Aventuras', 'Biográfico', 'Bélico', 'Ciencia Ficción', 'Comedia', 'Crimen', 'Drama', 'Fantastico', 'Infantil', 'Intriga', 'Melodrama', 'Musical', 'Romance', 'Sobrenatural', 'Suspense', 'Terror', 'Thriller', 'Western'],
   resolutions: ['1080p', '720p'],
   codecs: ['x265', 'x264'],
-  apiUrl: 'https://pelis-series-backend.onrender.com/api/'
+  apiUrl: 'http://localhost:3900/api/'
 };
 
 /*
